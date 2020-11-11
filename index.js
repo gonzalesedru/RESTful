fa559aef6c589ce2751c2cb6b3785813c5188411
edru gonzales <gonzales.edru@gmail.com>
2020-11-11 16:49:07 +0800

    Done CRUD

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require("express")
 const cors = require("cors")
+const mysql = require('mysql')
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'inventory'
+})
 
+con.connect((err) => {
+	if(err){
+		console.log(err)
+	}
+	else
+	{
+		console.log("connect to MySql DB")
+	}
+
+})
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -10,18 +27,43 @@ app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
 //create
-app.post('/post', (req, res) => {
-	console.log("post")
+app.post('/create', (req, res) => {
+	const sqlCreate = 'INSERT INTO items VALUES (?)'
+	const values = [
+		req.body.id,req.body.name,req.body.qty,req.body.amount
+	]
+	   con.query(sqlCreate, [values], (err, res) => {
+	      	if (err) throw err;  
+			console.log("Record Inserted");  
+	   });
 })
-app.get('/get', (req, res) => {
-	console.log("get")
+app.get('/run', (req, res) => {
+	const sqlRun = "SELECT * FROM items"
+	con.query(sqlRun, (err , res) => {
+		console.log(res)
+	})
 })
 app.put('/update', (req, res) => {
-	console.log("update")
+	const sqlUpdate = "UPDATE items SET ? WHERE id = ?";
+	const values = {
+		"name": req.body.name,
+		"qty": req.body.qty,
+		"amount": req.body.amount
+	}
+
+	
+	 con.query(sqlUpdate, [values, req.body.id], (err, res) => {
+	   if (err) throw err;
+	   console.log(res.affectedRows + " record(s) updated");
+	 });
 })
 
 app.delete('/delete', (req, res) => {
-	console.log("delete")
+	const sqlDelete = "DELETE FROM items WHERE id = ?";
+	  con.query(sqlDelete, req.body.id, (err, result) => {
+	    if (err) throw err;
+	    console.log("Number of records deleted: " + result.affectedRows);
+	  });
 })
 
 app.listen(port, () => console.log(`Listening to port ${port}`))
